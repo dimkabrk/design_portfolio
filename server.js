72ce65d9453dd13df83132bf166479eb06867ee0
NeyRon <brkdima7@gmail.com>
2026-07-03 02:36:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express=require('express');const path=require('path');const app=express();const PORT=process.env.PORT||3000;
-app.use(express.static(path.join(__dirname)));app.use('/public',express.static(path.join(__dirname,'public')));
-app.get('*',(req,res)=>{res.sendFile(path.join(__dirname,'index.html'));});
+app.use('/public',express.static(path.join(__dirname,'public')));app.use(express.static(__dirname));
+app.get('*',(req,res)=>{res.sendFile(path.resolve(__dirname,'index.html')),(err)=>{if(err)res.status(500).send('Missing index.html')}});
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`);});
